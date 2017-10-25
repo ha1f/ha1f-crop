af29252cbaf93cbe32388d86b77d494289b7d814
ha1f <ha1f@linecorp.com>
2017-10-25 18:25:41 +0900

    add comments

--- ha1f-crop/UIImage+Extension.swift
+++ ha1f-crop/UIImage+Extension.swift
@@ -12,40 +12,75 @@ extension CGFloat {
     func degreesToRadians() -> CGFloat {
         return self * CGFloat.pi / 180
     }
+    func radiansToDegrees() -> CGFloat {
+        return self / CGFloat.pi * 180
+    }
 }
 
 // この透過カットやってみたい
 // https://github.com/tomohisa/UIImage-Trim
 extension UIImage {
-    func cropped(to rect: CGRect) -> UIImage? {
-        var cgImage: CGImage? = self.cgImage
-        if cgImage == nil {
-            if let ciImage = self.ciImage ?? CIImage(image: self) {
-                let context = CIContext(options: nil)
-                cgImage = context.createCGImage(ciImage, from: ciImage.extent)
-            }
+    var safeCiImage: CIImage? {
+        return self.ciImage ?? CIImage(image: self)
+    }
+    
+    var safeCgImage: CGImage? {
+        if let cgImge = self.cgImage {
+            return cgImge
         }
-        guard let unwrappedCGImage = cgImage else {
-            print("converting cgimage error")
+        if let ciImage = safeCiImage {
+            let context = CIContext(options: nil)
+            return context.createCGImage(ciImage, from: ciImage.extent)
+        }
+        return nil
+    }
+    
+    /// Create UIImage by cropping current image with the specified rectangle.
+    ///
+    /// - parameter rect: The rectangle to crop
+    ///
+    /// - returns: The cropped image. Nil on error.
+    func cropped(to rect: CGRect) -> UIImage? {
+        guard let cgImage = safeCgImage else {
+            debugPrint("converting cgimage error")
             return nil
         }
-        guard let imgRef = unwrappedCGImage.cropping(to: rect) else {
-            print("cropping error")
+        guard let imgRef = cgImage.cropping(to: rect) else {
+            debugPrint("cropping error")
             return nil
         }
         return UIImage(cgImage: imgRef, scale: scale, orientation: imageOrientation)
     }
     
+    /// Create UIImage by rotating current image and crop center to keep image-size.
+    /// The image size may not be changed
     /// https://stackoverflow.com/questions/40389215/swift-rotate-an-image-then-zoom-in-and-crop-to-keep-width-height-aspect-ratio
+    ///
+    /// - parameter angle: Angle to rotate
+    ///
+    /// - returns: The rotated image. Nil on error.
     func rotatedAndCropped(angle: CGFloat) -> UIImage? {
-        let ciImage = self.ciImage ?? CIImage(image: self)
-        let rotated = ciImage?.applyingFilter("CIStraightenFilter", parameters: [kCIInputAngleKey: -angle])
-        return rotated.map { UIImage(ciImage: $0) }
+        guard let ciImage = safeCiImage else {
+            return nil
+        }
+        let rotated = ciImage.applyingFilter("CIStraightenFilter", parameters: [kCIInputAngleKey: -angle])
+        return UIImage(ciImage: rotated)
     }
     
+    
+    /// Create UIImage by rotating current image.
+    /// The image size may changed to covert as rectangle
     /// http://blogs.innovationm.com/image-croprotateresize-handling-in-ios/
-    func rotated(angle: CGFloat, flipVertical: CGFloat = 0.0, flipHorizontal: CGFloat = 0.0) -> UIImage? {
-        let ciImage = self.ciImage ?? CIImage(image: self)
+    ///
+    /// - parameter angle: Angle to rotate
+    /// - parameter flipVertical: Whether the image will be flipped vertically or not. Defaults to false
+    /// - parameter flipHorizontal: Whether the image will be flipped horizontally or not. Defaults to false
+    ///
+    /// - returns: The rotated image. Nil on error.
+    func rotated(angle: CGFloat, flipVertical: Bool = false, flipHorizontal: Bool = false) -> UIImage? {
+        guard let ciImage = safeCiImage else {
+            return nil
+        }
         
         guard let filter = CIFilter(name: "CIAffineTransform") else {
             return nil
@@ -57,27 +92,137 @@ extension UIImage {
         
         var transform = CATransform3DIdentity
         transform = CATransform3DRotate(transform, CGFloat(newAngle), 0, 0, 1)
-        transform = CATransform3DRotate(transform, flipVertical * CGFloat.pi, 0, 1, 0)
-        transform = CATransform3DRotate(transform, flipHorizontal * CGFloat.pi, 1, 0, 0)
+        transform = CATransform3DRotate(transform, (flipVertical ? 1.0 : 0) * CGFloat.pi, 0, 1, 0)
+        transform = CATransform3DRotate(transform, (flipHorizontal ? 1.0 : 0) * CGFloat.pi, 1, 0, 0)
         
         let affineTransform = CATransform3DGetAffineTransform(transform)
         
-        filter.setValue(NSValue(cgAffineTransform: affineTransform), forKey: "inputTransform")
-        
-        let contex = CIContext(options: [kCIContextUseSoftwareRenderer: true])
+        filter.setValue(NSValue(cgAffineTransform: affineTransform), forKey: kCIInputTransformKey)
         
         guard let outputImage = filter.outputImage else {
             return nil
         }
-        guard let cgImage = contex.createCGImage(outputImage, from: outputImage.extent) else {
+        let context = CIContext(options: [kCIContextUseSoftwareRenderer: true])
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {
             return nil
         }
         
         return UIImage(cgImage: cgImage)
     }
     
-    static func empty(size: CGSize, color: UIColor = .clear) -> UIImage? {
+    /// Create UIImage by drawing current image on colored context.
+    ///
+    /// - parameter color: Color of the background context
+    ///
+    /// - returns: The created image. Nil on error.
+    func withSettingBackground(color: UIColor) -> UIImage? {
         UIGraphicsBeginImageContext(size)
+        defer {
+            UIGraphicsEndImageContext()
+        }
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return nil
+        }
+        guard let cgImage = safeCgImage else {
+            return nil
+        }
+        let frame = CGRect(origin: .zero, size: size)
+        context.clear(frame)
+        context.setFillColor(color.cgColor)
+        context.fill(frame)
+        context.draw(cgImage, in: frame)
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
+    
+    /// Create UIImage of black or transparent.
+    /// CIImage-masking treats white as transparent, while CALayer-masking
+    /// treats transparent as transparent.
+    /// For this reason, we should make white of UIImage transparent to use
+    /// the CIImage-masking image for CALayer-masking image.
+    ///
+    /// - parameter inverse: Invert transparent area or not
+    ///
+    /// - returns: The created image. Nil on error.
+    func blacked(inverse: Bool = false) -> UIImage? {
+        if inverse {
+            guard let mask = UIImage.empty(size: size, color: .white)?.masked(with: self)?.withSettingBackground(color: .black) else {
+                return nil
+            }
+            return UIImage.empty(size: size, color: .black)?.masked(with: mask)
+        } else {
+            return UIImage.empty(size: size, color: .black)?.masked(with: self)
+        }
+    }
+    
+    /// Create UIImage by masking current image with another image.
+    /// Treat white as transparent.
+    ///
+    /// - parameter image: Image for masking
+    ///
+    /// - returns: The created image. Nil on error.
+    func masked(with image: UIImage) -> UIImage? {
+        guard let maskRef = image.safeCgImage,
+            let ref = safeCgImage,
+            let dataProvider = maskRef.dataProvider else {
+                return nil
+        }
+        
+        let mask = CGImage(maskWidth: maskRef.width,
+                           height: maskRef.height,
+                           bitsPerComponent: maskRef.bitsPerComponent,
+                           bitsPerPixel: maskRef.bitsPerPixel,
+                           bytesPerRow: maskRef.bytesPerRow,
+                           provider: dataProvider,
+                           decode: nil,
+                           shouldInterpolate: false)
+        return mask
+            .flatMap { ref.masking($0) }
+            .map { UIImage(cgImage: $0) }
+    }
+    
+    func transformed(angle: CGFloat) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(size, true, UIScreen.main.scale)
+        defer {
+            UIGraphicsEndImageContext()
+        }
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return nil
+        }
+        guard let cgImage = safeCgImage else {
+            return nil
+        }
+        
+        let frame = CGRect(origin: .zero, size: size)
+        
+        // clear
+        context.clear(frame)
+        
+        // background
+        context.setFillColor(UIColor.black.cgColor)
+        context.fill(frame)
+        
+        // rotate
+        context.translateBy(x: frame.midX, y: frame.midY)
+        context.scaleBy(x: 1.0, y: -1.0)
+        context.rotate(by: angle)
+        
+        // image
+        context.draw(cgImage, in: frame.offsetBy(dx: -frame.midX, dy: -frame.midY))
+        
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
+}
+
+extension UIImage {
+    
+    /// Create UIImage filled with a color.
+    ///
+    /// - parameter size: Size of output image
+    /// - parameter color: Color to fill
+    ///
+    /// - returns: The created image. Nil on error.
+    static func empty(size: CGSize, color: UIColor = .clear) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(size, true, UIScreen.main.scale)
         defer {
             UIGraphicsEndImageContext()
         }
@@ -91,8 +236,15 @@ extension UIImage {
         return UIGraphicsGetImageFromCurrentImageContext()
     }
     
+    /// Create UIImage of circle.
+    ///
+    /// - parameter size: Size of output image
+    /// - parameter color: Color of the circle
+    /// - parameter backgroundColor: Background color of the image
+    ///
+    /// - returns: The created image. Nil on error.
     static func circle(size: CGSize, color: UIColor, backgroundColor: UIColor = .clear) -> UIImage? {
-        UIGraphicsBeginImageContext(size)
+        UIGraphicsBeginImageContextWithOptions(size, true, UIScreen.main.scale)
         defer {
             UIGraphicsEndImageContext()
         }
@@ -115,55 +267,34 @@ extension UIImage {
         return UIGraphicsGetImageFromCurrentImageContext()
     }
     
-    func withSettingBackground(color: UIColor) -> UIImage? {
-        UIGraphicsBeginImageContext(size)
-        defer {
-            UIGraphicsEndImageContext()
-        }
+    /// Create UIImage by drawing text.
+    ///
+    /// - parameter text: string to draw
+    /// - parameter fontSize: size of text
+    ///
+    /// - returns: The created image. Nil on error.
+    static func fromText(text: String, fontSize: CGFloat) -> UIImage? {
+        let attributes = [
+            NSAttributedStringKey.font: UIFont.systemFont(ofSize: fontSize),
+            NSAttributedStringKey.foregroundColor: UIColor.white,
+            NSAttributedStringKey.paragraphStyle: NSMutableParagraphStyle.default
+        ]
+        let imageSize = (text as NSString).size(withAttributes: attributes)
+        
+        UIGraphicsBeginImageContextWithOptions(imageSize, false, UIScreen.main.scale)
         guard let context = UIGraphicsGetCurrentContext() else {
             return nil
         }
-        let frame = CGRect(origin: .zero, size: size)
-        context.clear(frame)
-        context.setFillColor(color.cgColor)
-        context.fill(frame)
-        context.draw(self.cgImage!, in: frame)
-        return UIGraphicsGetImageFromCurrentImageContext()
-    }
-    
-    /// black or transparent
-    func blacked(inverse: Bool = false) -> UIImage? {
-        if inverse {
-            guard let mask = UIImage.empty(size: size, color: .white)?.masked(with: self)?.withSettingBackground(color: .black) else {
-                return nil
-            }
-            return UIImage.empty(size: size, color: .black)?.masked(with: mask)
-        } else {
-            return UIImage.empty(size: size, color: .black)?.masked(with: self)
-        }
-    }
-    
-    func withTransparentWhite() -> UIImage? {
-        return self.masked(with: self)
-    }
-    
-    func masked(with image: UIImage) -> UIImage? {
-        guard let maskRef = image.cgImage,
-            let ref = cgImage,
-            let dataProvider = maskRef.dataProvider else {
-                return nil
-        }
         
-        let mask = CGImage(maskWidth: maskRef.width,
-                           height: maskRef.height,
-                           bitsPerComponent: maskRef.bitsPerComponent,
-                           bitsPerPixel: maskRef.bitsPerPixel,
-                           bytesPerRow: maskRef.bytesPerRow,
-                           provider: dataProvider,
-                           decode: nil,
-                           shouldInterpolate: false)
-        return mask
-            .flatMap { ref.masking($0) }
-            .map { UIImage(cgImage: $0) }
+        context.setTextDrawingMode(CGTextDrawingMode.fill)
+        
+        let textRect = CGRect(origin: .zero, size: imageSize)
+        text.draw(in: textRect, withAttributes: attributes)
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        return newImage
     }
 }
